e213f940af13cc1a4615113303520e73f9342133
unique2721 <uniquehabeshawi@gmail.com>
2025-01-05 22:43:02 +0300

    #

--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -63,10 +63,10 @@ const Cars = () => {
               className="w-full h-56 rounded-t-xl object-cover group-hover:brightness-90 transition-all duration-300"
             />
             <div className="p-6">
-              <h2 className=" font-semibold text-gray-800 text-xl mb-4">
+              <h2 className=" font-bold text-gray-800 text-xl mb-4">
                 {car.name}
-                <h2>location goes here</h2>
               </h2>
+                <h2 className='mb-6'>location goes here</h2>
               <div className=" flex flex-col justify-center items-center mb-6">
                 <div className='flex space-x-28 mb-2'>
                 <div className="flex justify-center items-center gap-2 text-gray-700">
