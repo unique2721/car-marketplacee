646f8d247c0aaa1235a91bfb6fa7bb06efcb3758
unique2721 <uniquehabeshawi@gmail.com>
2025-01-05 21:33:03 +0300

    #

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,7 +5,7 @@ const Category = () => {
   return (
     <div className='mt-20'>
       <h2 className=' font-bold text-3xl text-center mb-6 '>Browse By Type</h2>
-      <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-9 gap-6 px-20'>
+      <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-9 gap-6 px-20'>
         {
           Data.Category.map((category,index) => (
             <div key={index} className='border rounded-xl p-3 flex flex-col items-center hover:shadow-md cursor-pointer'>
