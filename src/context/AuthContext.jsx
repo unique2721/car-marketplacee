7cbcfe3bfed1cfc592636144e9087f82f211ebbd
unique2721 <uniquehabeshawi@gmail.com>
2025-01-08 23:12:55 +0300

    #

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
-import { mockUsers } from "../data/mockData";
+import { mockUsers } from "../Data/mockData";
 
 const AuthContext = createContext(undefined);
 
