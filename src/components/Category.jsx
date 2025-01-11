7abe44f540224b9422a651d57abe2eb3a7c34920
unique2721 <uniquehabeshawi@gmail.com>
2025-01-11 19:53:29 +0300

    #

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,7 +3,7 @@ import Data from "../Data/Data";
 
 const Category = () => {
   return (
-    <div className="mt-20">
+    <div className="my-20">
       <h2 className=" font-bold text-3xl text-center mb-6 ">Browse By Type</h2>
       <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-9 gap-6 px-20">
         {Data.Category.map((category, index) => (
