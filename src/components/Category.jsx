c48a10f91cd950fd4f504573591dbd6391286be6
unique2721 <uniquehabeshawi@gmail.com>
2025-01-13 18:01:32 +0300

    #

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -12,11 +12,11 @@ const Category = () => {
       }}
     >
       <h2 className=" font-bold text-3xl text-center mb-10 text-white ">Browse Cars By Type</h2>
-      <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-9 gap-6 px-20">
+      <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-9 gap-4 px-20">
         {Data.Category.map((category, index) => (
           <div
             key={index}
-            className="border bg-white text-black rounded-xl p-3 flex flex-col items-center hover:shadow-md hover:bg-slate-100 cursor-pointer"
+            className="border bg-white text-black rounded-xl py-3 flex gap-2 justify-center items-center hover:shadow-md hover:bg-slate-100 cursor-pointer"
           >
             <img
               className=" bg-white"
