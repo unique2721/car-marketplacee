d5fa76b584509144a7dbbe3a1c2d4ede46b0f123
unique2721 <uniquehabeshawi@gmail.com>
2025-01-13 18:12:07 +0300

    #

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -11,20 +11,17 @@ const Category = () => {
           "url(https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?w=1920)",
       }}
     >
-      <h2 className=" font-bold text-3xl text-center mb-10 text-white ">Browse Cars By Type</h2>
-      <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-9 gap-6 px-20">
+      <h2 className=" font-bold text-3xl text-center mb-10 text-white ">
+        Browse Cars By Type
+      </h2>
+      <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-9 gap-4 px-20">
         {Data.Category.map((category, index) => (
           <div
             key={index}
-            className="border bg-white text-black rounded-xl p-3 flex justify-center items-center gap-2 hover:shadow-md hover:bg-slate-100 cursor-pointer"
+            className="border bg-white text-black rounded-xl p-1 flex flex-wrap justify-center items-center gap-2 hover:shadow-md hover:bg-slate-100 cursor-pointer"
           >
-            <img
-              src={category.icon}
-              alt="brand"
-              width={20}
-              height={20}
-            />
-            <h2 className="mt-2 font-semibold">{category.name}</h2>
+            <img src={category.icon} alt="brand" width={20} height={20} />
+            <h2 className=" font-semibold">{category.name}</h2>
           </div>
         ))}
       </div>
